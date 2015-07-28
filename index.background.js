78d5259f0ac9333ec0f99d669f8d40ea262dab91
Ryan Bennett <nomad.ry@gmail.com>
2015-07-28 15:19:46 -0600

    initial

---
+++ index.background.js
@@ -0,0 +1,35 @@
+
+window.http = require("http-chrome");
+window.wiki = require("./tmp/server.js");
+window.fs = require("browserify-fs");
+window._http = require("http")
+window.teletunnel = require("teletunnel")
+window.trie = require("trie-search")
+window.app = wiki({_isChrome:true
+                , root: "/"
+                , data: "data"
+                , packageDir:"node_modules"
+                , _nolog:true
+                , _main: function(req,res){
+                    fs.createReadStream("./node_modules/wiki-client/views/static.html")
+                      .pipe(res)
+                    }
+                })
+
+chrome.app.runtime.onLaunched.addListener(function() {
+  window.serv = http.createServer(app)
+  serv.listen(3000,function(){
+    chrome.app.window.create('index.html', {
+      id: 'MyWindowID',
+      bounds: {
+        width: 800,
+        height: 600,
+        left: 100,
+        top: 100
+      },
+      minWidth: 800,
+      minHeight: 600
+    });
+  })
+
+});
